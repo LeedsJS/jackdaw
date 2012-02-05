8fdb92b6a2c1fa8ef0a41442da31ab550a8e93b5
sfoster <sam@sam-i-am.com>
2012-02-05 09:14:59 +0000

    Failing tests for a basic entity-component system

---
+++ tests/spec/componentEntitySpec.js
@@ -0,0 +1,48 @@
+define(['lib/components', 'lib/entity'], function(componentRegistry, Entity){
+
+	describe("component registry", function() {
+
+		it("should allow a component to be registered", function() {
+		});
+
+		it("should return a registered component given its name", function() {
+		});
+
+  });
+
+	describe("entity is a class", function() {
+
+		it("should be a function", function() {
+		});
+
+		it("should create instances", function() {
+		});
+
+		it("should accept a list of components", function() {
+		});
+
+		it("should throw on bogus or unregistered components", function() {
+		});
+
+	});
+
+	describe("entity init", function() {
+
+		it("should call all components with the instance", function(){
+		  
+		});
+
+	});
+
+	describe("component", function() {
+
+		it("should have an init method", function(){
+		  
+		});
+
+		it("can implement key lifcycle methods", function(){
+		  
+		});
+
+	});
+});
